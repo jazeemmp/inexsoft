439f0164295cb81b44f75b5edbf314f73bfff5c9
jazeem <jazeemmp404@gmail.com>
2025-02-23 22:47:58 +0530

    bug fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import mongoConnect from "./config/mongoConnnect.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 dotenv.config();
 mongoConnect();
@@ -16,4 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", userRoutes);
 app.use("/api/admin", adminRoutes);
 
-export default app; // ✅ Just export the Express app
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res); // This invokes the express app for serverless function handling
+};
