c0b7860c58feb14472eb2aadec4d6a98841c7742
jazeem <jazeemmp404@gmail.com>
2025-02-23 22:53:28 +0530

    bug fixes

--- admin/src/api/axios.ts
+++ admin/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backend-f470lga4h-jazeemmps-projects.vercel.app/api/admin',
+    baseURL: 'https://backend-beta-lake-45.vercel.app/api/admin',
     withCredentials: true,
 });
 export default api;
--- client/src/apis/axios.ts
+++ client/src/apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-f470lga4h-jazeemmps-projects.vercel.app/api/",
+  baseURL: "https://backend-beta-lake-45.vercel.app/api/",
   withCredentials: true,
 });
 
